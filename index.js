802be530e26c2ccf152c3768aae85190832fd478
Charles Rethman <rethman@me.com>
2016-08-31 11:21:18 +0200

    Added in new index.js, pg_connect.js, mongo_connect.js files; plan to split up code in scraper.js into functional modules and augment it

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+/*
+ * @file_name: index.js
+ *
+ */
+
+// require local modules: connect to db, spreadsheet scraper
+var db = require('pg_connector.js'), scraper = require('scraper.js');
+
+
+// call module for connecting to db
