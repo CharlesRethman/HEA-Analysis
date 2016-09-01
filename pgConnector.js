45207974f20c1d39f4c3ab78ed5bc3a9405e56e5
Charles Rethman <rethman@me.com>
2016-09-01 11:23:14 +0200

    Renamed pg_connect.js to PgConnect.js & updated it to become prototypes for calling function to create objects on, updated index.js to call PgConnect properly

--- pgConnector.js
+++ pgConnector.js
@@ -296,27 +296,6 @@ PgConnector.prototype.client = function(connString) {
    return new pg.Client(connString);
 }
 
-/*
- * Callers for getting user inputs for connecting to the database. Upon entry of credentials, the
- * `connectDB` function is called with the a client object for connecting to the database. No
- * authentication/authorisation at this stage.
- *
- */
-// Get the DB user name
-PgConnector.prototype.ask('\nYou may need account credentials to connect to Postgres. However, if this is not\nthe case, you can ignore the login role or the password below by just pressing\nENTER on each.\n\nPostgres user name', /.+|\s/, function(cancel, user_name) {
-   if (!cancel) {
-      // Get the DB password
-      getPassword('Postgres password', function(cancel, password) {
-         // pass the user name and password as a connection string onto Postgres in the main data
-         // processing function
-         if (!cancel) {
-            var client = new pg.Client('postgres://' + user_name + ':' + password + '@localhost:5432/albers_ea');
-            // Go through to slecting the month and year of analysis
-            connectDB(client);
-         }
-      });
-   }
-});
 
 
 exports.PgConnector = PgConnector;
