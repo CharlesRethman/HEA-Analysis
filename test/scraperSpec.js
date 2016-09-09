f6f2e618baed8834cc903f220860e6339ea783bb
Charles Rethman <rethman@me.com>
2016-09-09 16:14:45 +0200

    Started working on the scraper test

--- test/scraperSpec.js
+++ test/scraperSpec.js
@@ -1,1 +1,17 @@
-//Test for the spreadhseet scraper
+/*
+ *
+ * Test for the spreadhseet scraper
+ */
+
+
+var expect = require('chai').expect;
+var tags = require('../lib/scaper.js');
+
+describe('Scraper', function() {
+   describe('#sheets', function() {
+      it('should check all the sheets and look for the analysis sheets for each wealth group', function() {
+         var args = []
+
+      });
+   });
+});
