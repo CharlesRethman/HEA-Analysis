95b99994160fbd7365c4c0d6a5e6dbd8e6645040
Charles Rethman <rethman@me.com>
2016-09-09 17:58:25 +0200

    Started working on the scraper test

--- test/scraperSpec.js
+++ test/scraperSpec.js
@@ -9,8 +9,29 @@ var tags = require('../lib/scaper.js');
 
 describe('Scraper', function() {
    describe('#sheets', function() {
-      it('should check all the sheets and look for the analysis sheets for each wealth group', function() {
-         var args = []
+      it('should get a list of all the sheets', function() {
+         var args = [
+            '../data/spreadsheets/za_fw_0.xlsx',
+            '../data/spreadsheets/za_fw_0_nogrants.xlsx',
+            '../data/spreadsheets/za_fw_1.xlsx',
+            '../data/spreadsheets/za_fw_1_nogrants.xlsx',
+            '../data/spreadsheets/za_up_0.xlsx',
+            '../data/spreadsheets/za_up_0_nogrants.xlsx',
+            '../data/spreadsheets/za_up_1.xlsx',
+            '../data/spreadsheets/za_up_1_nogrants.xlsx',
+            '../data/spreadsheets/za1xx_0.xlsx',
+            '../data/spreadsheets/za1xx_0_nogrants.xlsx',
+            '../data/spreadsheets/za1xx_1.xlsx',
+            '../data/spreadsheets/za1xx_1_nogrants.xlsx',
+            '../data/spreadsheets/za2xx_0.xlsx',
+            '../data/spreadsheets/za2xx_0_nogrants.xlsx',
+            '../data/spreadsheets/za2xx_1.xlsx',
+            '../data/spreadsheets/za2xx_1_nogrants.xlsx',
+            '../data/spreadsheets/za3xx_0.xlsx',
+            '../data/spreadsheets/za3xx_0_nogrants.xlsx',
+            '../data/spreadsheets/za3xx_1.xlsx',
+            '../data/spreadsheets/za3xx_1_nogrants.xlsx'
+         ]
 
       });
    });
