e66ed73ff72dc9044459d06b8d340a5981d02a6e
Charles Rethman <rethman@me.com>
2016-09-12 18:06:10 +0200

    writing first indexSpec.js test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -5,11 +5,19 @@
 
 
 var expect = require('chai').expect;
-var scraper = require('../index.js');
+var webdriver = require('selenium-WebDriver'), By = webdriver.By, until = webdriver.until;
+
 
 describe('Index', function() {
    it('should start the http server and load a box with a label and a browse button', function(){
       this.timeout(3000);
+      var driver = new webdriver.Builder().forBrowser('firefox').buid();
+      driver.get('http://localhost:3000');
 
+      console.log(driver.findElement(By.name('lblLoad')));
+      driver.findElement(By.name('lblLoad'), function(err, text) {
+         expect(text).to.equal('Choose your spreadsheet or folder of spreadsheets')
+      });
+      driver.quit();
    });
 });
