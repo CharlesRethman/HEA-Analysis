ac729976e140dbdabb687f083f50abce3770e31d
Charles Rethman <rethman@me.com>
2016-09-13 00:32:45 +0200

    working first indexSpec.js test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -3,26 +3,24 @@
  * Test for the spreadhseet scraper
  */
 
-
-var expect = require('chai').expect;
-var webdriver = require('selenium-webdriver')
-
-before ->
-   @timeout 10000;
-   @driver = new selenium.Builder()
-, By = webdriver.By, until = webdriver.until;
+var chai = require('chai'), expect = chai.expect;
+//var chai.use = require('chai-as-promised');
+var webdriver = require('selenium-webdriver');
 
 
 describe('Index', function() {
-   it('should start the http server and load a box with a label and a browse button', function(){
-      this.timeout(3000);
-      var driver = new webdriver.Builder().forBrowser('firefox').buid();
-      driver.get('http://localhost:3000');
-
-      console.log(driver.findElement(By.name('lblLoad')));
-      driver.findElement(By.name('lblLoad'), function(err, text) {
-         expect(text).to.equal('Choose your spreadsheet or folder of spreadsheets')
+   it('should work', function() {
+      this.timeout(10000);
+      var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+      driver.get('http://localhost:3000', function() {
+         expect(this.getTitle()).to.equal('Spreadsheet Analyser');
+         var pathBox = this.findElement(webdriver.By.name('path'));
+         pathBox.sendKeys(process.env.PWD + '/data/spreadsheets/');
+         pathBox.getAttribute('value').then(function(value) {
+            expect(value).to.equal(process.env.PWD + '/data/spreadsheets');
+         });
       });
-      driver.quit();
+
+//      driver.quit();
    });
 });
