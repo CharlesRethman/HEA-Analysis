4e665010ebd54ed1da3b5ece4070d33fc8550351
Charles Rethman <rethman@me.com>
2016-09-13 01:19:41 +0200

    working first indexSpec.js test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -7,19 +7,21 @@ var chai = require('chai'), expect = chai.expect;
 //var chai.use = require('chai-as-promised');
 var webdriver = require('selenium-webdriver');
 
+function enterBox(driver) {
+   var pathBox = driver.findElement(webdriver.By.name('path'));
+   pathBox.sendKeys(process.env.PWD + '/data/spreadsheets/');
+   return pathBox.getAttribute('value');
+}
+
 
 describe('Index', function() {
    it('should work', function() {
       this.timeout(10000);
       var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-      driver.get('http://localhost:3000', function() {
-         expect(this.getTitle()).to.equal('Spreadsheet Analyser');
-         var pathBox = this.findElement(webdriver.By.name('path'));
-         pathBox.sendKeys(process.env.PWD + '/data/spreadsheets/');
-         pathBox.getAttribute('value').then(function(value) {
-            expect(value).to.equal(process.env.PWD + '/data/spreadsheets');
-         });
-      });
+      driver.get('http://localhost:3000');
+      expect(driver.getTitle()).to.equal('Spreadsheet Analyser');
+      expect(driver.findElement(webdriver.By.name('btn')).getAttribute('text')).to.equal('Browse...')
+      expect(enterBox(driver)).to.equal(process.env.PWD + '/data/spreadsheets');
 
 //      driver.quit();
    });
