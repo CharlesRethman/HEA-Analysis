346af4b89b8cffbd58ee4d142d6d5976aea4e7f4
Charles Rethman <rethman@me.com>
2016-09-14 08:39:32 +0200

    working first indexSpec.js test

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -3,26 +3,25 @@
  * Test for the spreadhseet scraper
  */
 
-var chai = require('chai'), expect = chai.expect;
-//var chai.use = require('chai-as-promised');
-var webdriver = require('selenium-webdriver');
-
-function enterBox(driver) {
-   var pathBox = driver.findElement(webdriver.By.name('path'));
-   pathBox.sendKeys(process.env.PWD + '/data/spreadsheets/');
-   return pathBox.getAttribute('value');
-}
+var expect = require('chai').expect,
+   webdriver = require('selenium-webdriver'),
+   test = require('selenium-webdriver/testing')
 
 
-describe('Index', function() {
-   it('should work', function() {
-      this.timeout(10000);
-      var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+test.describe('Index', function() {
+   this.timeout(15000);
+   var driver;
+
+   test.before(function() {
+      driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+   });
+
+   test.it('should work', function() {
       driver.get('http://localhost:3000');
-      expect(driver.getTitle()).to.equal('Spreadsheet Analyser');
-      expect(driver.findElement(webdriver.By.name('btn')).getAttribute('text')).to.equal('Browse...')
-      expect(enterBox(driver)).to.equal(process.env.PWD + '/data/spreadsheets');
 
-//      driver.quit();
+   });
+
+   test.after(function() {
+      driver.quit();
    });
 });
