03698d5b7b2f798baaee72b62db00b049c46e54c
Charles Rethman <rethman@me.com>
2016-09-15 13:54:56 +0200

    worked on index.js, indexSpec.js test and html view files

--- index.js
+++ index.js
@@ -6,3 +6,33 @@
  */
 
 // Load required packages
+
+var express = require('express'),
+   http = require('http'),
+//   bodyParser = require('body-parser'),
+   passport = require('passport');
+
+
+// Create our Express application
+var app = express();
+app.set('port', 3000);
+app.use(express.static('views'));
+
+
+// Use the body-parser package in our application
+//app.use(bodyParser.urlencoded({
+// 	extended: true
+//}));
+
+// respond to GET with a small piece of HTML
+app.get('/lolto', (req, res) => {
+   res.send('<html><body><h1>It worked!</h1></body></html>')
+});
+
+// Create our Express router
+//var router = express.Router();
+
+// Start the server
+http.createServer(app).listen(app.get('port'), () => {
+   console.log('Server listening on port ' + app.get('port'));
+});
--- test/indexSpec.js
+++ test/indexSpec.js
@@ -16,20 +16,28 @@ test.describe('Index', function() {
       driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
    });
 
-   test.it('should open the browser', () => {
+   test.it('should open the browser', function() {
       driver.get('http://localhost:3000');
 //      driver.wait(function() {
 //      }, 3000);
    });
 
-   test.it('should retrieve the heading', () => {
+   test.it('should retrieve the heading', function() {
       driver.findElement(webdriver.By.tagName('h1')).getText().then(function(text) {
 //         console.log('Text is:', text);
-         expect(text).to.equal('It worked!');
+         expect(text).to.equal('HEA Analysis Spreadsheet loader');
       });
    });
 
-   test.after(() => {
+   test.it('should have a single-line text input box', function() {
+      driver.findElement(webdriver.By.name('pathToFile').sendKeys('/Users/Charles/Documents/hea_analysis/south_africa/2016.04/spreadsheets/za2xx_0.xlsx');
+   })
+
+   test.it('should a have a submit button', function() {
+      driver.findElement(webdriver.By.name('loadFile').)
+   })
+
+   test.after(function() {
       driver.quit();
    });
 });
