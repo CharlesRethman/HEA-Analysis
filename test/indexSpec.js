68e1591d9cc8a9c90ceea7fb41e807b544bd50c2
Charles Rethman <rethman@me.com>
2016-09-16 01:02:26 +0200

    worked on index.js, indexSpec.js test and pug and html view files

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -5,7 +5,8 @@
 
 var expect = require('chai').expect,
    webdriver = require('selenium-webdriver'),
-   test = require('selenium-webdriver/testing')
+   test = require('selenium-webdriver/testing'),
+   path = '/Users/Charles/Documents/hea_analysis/south_africa/2016.04/spreadsheets/za2xx_1.xlsx'
 
 
 test.describe('Index', function() {
@@ -44,17 +45,23 @@ test.describe('Index', function() {
       element.getAttribute('size').then(function(value) {
          expect(value).to.equal("60");
       });
-//      element.sendKeys('/Users/Charles/Documents/hea_analysis/south_africa/2016.04/spreadsheets/za2xx_1.xlsx');
+//      element.sendKeys(path);
    });
 
    test.it('should have a \'Choose file\' button', function() {
       element = driver.findElement(webdriver.By.id('btnChooseFile'));
-      element.getAttribute("accept").then(function(text) {
+      element.getAttribute('accept').then(function(text) {
          var arr = text.split(',');
          arr = arr.sort();
          expect(arr).to.deep.equal(['.xls','.xlsx']);
       });
-      element.sendKeys('/Users/Charles/Documents/hea_analysis/south_africa/2016.04/spreadsheets/za2xx_1.xlsx');
+      element.sendKeys(path).then(function() {
+         element.getAttribute('value').then(function(text) {
+            text = text.slice(text.lastIndexOf('\\') + 1);
+            path = path.slice(path.lastIndexOf('/') + 1);
+            expect(text).to.equal(path);
+         });
+      });
    });
 
    test.it('should a have an \'Upload\' button', function() {
