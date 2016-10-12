7b1a6fa4cb5b3169f3c3bf2cd365d1a9d4a5b8f6
Charles Rethman <rethman@me.com>
2016-10-12 22:38:41 +0200

    Added in new test routine, routesSpec.js, and update appSpec.js

---
+++ test/routesSpec.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Test for the server index.js
+ */
+
+var expect = require('chai').expect,
+   index = require('../routes/index'),
+   uploads = require('../routes/uploads')
+
+describe('Routes', function() {
+   describe('Index', function() {
+      it('should conduct a match code on the DB lz_code field, abbreviation on the DB lz_abbrev field and do a full text search on the DB lz_name field', function() {
+         expect()
+         it()
+      });
+   });
+});
